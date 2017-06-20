02928fb0bc4bd890ad536f742d3fd792a2074e74
Elliott O'Hara <elliott.ohara@gmail.com>
2017-06-19 20:52:32 -0500

    no keys

--- backup.sh
+++ backup.sh
@@ -9,8 +9,8 @@ zip /home/ec2-user/backups/backup.zip /home/ec2-user/backups/backup.sql
 rm  /home/ec2-user/backups/backup.sql
 
 docker run \
---env aws_key=AKIAIBP3FR633F43H5XA \
---env aws_secret=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 \
+--env aws_key=MY KEY \
+--env aws_secret=MY SECRET \
 --env cmd=sync-local-to-s3 \
 --env DEST_S3=s3://media.bjjpedia.com/BACKUP/`date '+%Y/%m/%d/'` \
 -v /home/ec2-user/backups:/opt/src \
