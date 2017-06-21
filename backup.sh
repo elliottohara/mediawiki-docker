ff75193aee5e2be7bf424198dda6b61b8802b5cc
Elliott O'Hara <elliott.ohara@gmail.com>
2017-06-21 10:56:46 -0500

    using env variables

--- backup.sh
+++ backup.sh
@@ -11,8 +11,8 @@ zip /home/ec2-user/backups/backup.zip /home/ec2-user/backups/backup.sql
 rm  /home/ec2-user/backups/backup.sql
 
 docker run \
---env aws_key=${AWS_KEY} \
---env aws_secret=${AWS_SECRET} \
+--env aws_key=$AWS_KEY \
+--env aws_secret=$AWS_SECRET: \
 --env cmd=sync-local-to-s3 \
 --env DEST_S3=s3://media.bjjpedia.com/BACKUP/`date '+%Y/%m/%d/'` \
 -v /home/ec2-user/backups:/opt/src \
